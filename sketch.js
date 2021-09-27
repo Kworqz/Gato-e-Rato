114f78bb9bd5d3537ecec7fd22791d46a6dd507d
Kworqz <90353659+Kworqz@users.noreply.github.com>
2021-09-27 17:30:59 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,17 +1,18 @@
 var gato,rato;
+var backgroundImg, gatoImg1, ratoImg1, ratoImg2, gatoImg2, gatoImg3, ratoImg3;
 
 function preload() {
-    backgroundImg = loadImage("images/garden.png")
-    gatoImg1 = loadImage("images/cat1.png");
-    ratoImg1 = loadImage("images/mouse1.png");
-    gatoImg2 = loadImage("images/cat2.png")
-    ratoImg2 = loadImage("images/mouse2.png");
-    gatoImg3 = loadImage("images/cat3.png");
-    ratoImg3 = loadImage("images/mouse3.png");
+    backgroundImg = loadImage("images/garden.png");
+    gatoImg1 = loadAnimation("images/cat1.png");
+    ratoImg1 = loadAnimation("images/mouse1.png");
+    gatoImg2 = loadAnimation("images/cat2.png", "images/cat3.png");
+    ratoImg2 = loadAnimation("images/mouse2.png", "images/mouse3.png");
+    gatoImg3 = loadAnimation("images/cat4.png");
+    ratoImg3 = loadAnimation("images/mouse4.png");
 }
 
 function setup(){
-    createCanvas(1000,800);  
+    createCanvas(1000,800); 
     gato = createSprite(850,600);
     gato.addAnimation("gatoSentado",gatoImg1);
     rato = createSprite(250,600);
@@ -19,25 +20,23 @@ function setup(){
 }
 
 function draw() {
-
     background(backgroundImg);
-    if(gato.x - rato.x < (gato.width + rato.width)/2 ){
+    if(gato.x - rato.x < gato.width/2 + rato.width/2 ){
     gato.velocityX = 0;
     gato.addAnimation("gatofeliz",gatoImg3);
     gato.changeAnimation("gatofeliz");
-    gato.x = 300;   
+    rato.addAnimation("ratofeliz",ratoImg3);
+    rato.changeAnimation("ratofeliz");
+    //gato.x = 300;   
     }
    
     keyPressed();
-
-    
+   
     drawSprites();
-    }
-    
 
+}
 
 function keyPressed(){
-
     if(keyDown("left")){
 
         rato.addAnimation("ratoProvocando", ratoImg2);
